6e00b1f04bab95a25ae0c0fb2b676d3cf203511f
Ricky McCallum <rickymccallum87@gmail.com>
2019-01-17 18:27:20 -0500

    Pig latin

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,5 +1,25 @@
-function translate() {
-	// body...
+function translate(str) {
+	let words = str.split(' ');
+	const vowels = ['a','e','i','o','u'];
+
+	let arr;
+	for (let i = 0; i < words.length; i++) {
+		let arr = Array.from(words[i]);
+
+		if (vowels.includes(arr[0])) {
+			words[i] += 'ay';
+		} else {
+
+			do {
+				arr.push(arr.shift());
+			} while (!vowels.includes(arr[0])
+					|| arr[arr.length - 1] === 'q');
+
+			words[i] = arr.join('') + 'ay';
+		}
+	}
+
+	return words.join(' ');
 }
 
 
