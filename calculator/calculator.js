9ca4af41b7f9fbbba8dd2a2d8fcdf8e28c081add
Ricky McCallum <rickymccallum87@gmail.com>
2019-01-17 18:26:15 -0500

    Calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,42 @@
-function add () {
-	
+function add (a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (a) {
+	let total = 0;
+	for (let i = 0; i < a.length; i++) {
+		total += a[i];
+	}
+	return total;
 }
 
-function multiply () {
-	
+function multiply (nums) {
+	let total = nums[0];
+	for (let i = 1; i < nums.length; i++) {
+		total *= nums[i];
+	}
+	return total;
 }
 
-function power() {
-	
+function power(n, p) {
+	return n ** p;
 }
 
-function factorial() {
-	
+function factorial(n) {
+	if (n < 2) {
+		return 1;
+	} else {
+		let f = 1;
+		while (n >= 2) {
+			f *= n;
+			n--;
+		}
+		return f;
+	}
 }
 
 module.exports = {
