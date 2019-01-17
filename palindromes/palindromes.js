3e075b8f20cd28779cb8d227b43b8bc1f489170c
Ricky McCallum <rickymccallum87@gmail.com>
2019-01-17 18:27:11 -0500

    Palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function() {
+const palindromes = function(str) {
+    str = str.toLowerCase();
+    let arr = Array.from(str);
 
+    removeChars(['!',' ','.',','], arr);
+
+    let opp;
+    const half = Math.floor(str.length / 2);
+    for (let i = 0; i < half; i++) {
+        opp = arr.length - (i + 1);
+        if (arr[i] !== arr[opp]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function removeChars(chars, arr) {
+    for (let c = 0; c < chars.length; c++) {
+        while (arr.includes(chars[c])) {
+            arr.splice(arr.indexOf(chars[c]), 1);
+        }
+    }
 }
 
 module.exports = palindromes
