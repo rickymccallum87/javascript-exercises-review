bc3ddbbd2d3345db5acf3fcaace18d4b10d85279
Ricky McCallum <rickymccallum87@gmail.com>
2019-01-17 18:26:22 -0500

    Fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(m) {
+    if (m < 0) return 'OOPS';
+    let fib = [1,1];
+    m = +m;
+    for (let i = 2; i < m; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
+    }
 
+    return fib[m-1];
 }
 
 module.exports = fibonacci
