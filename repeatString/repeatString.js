1f9857cb03fa09fdcbd9d86a1a453c77c5ccb3fa
Ricky McCallum <rickymccallum87@gmail.com>
2019-01-17 18:27:39 -0500

    Repeat string

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(original, count) {
+    if (count < 0) return 'ERROR';
+    let repeated = '';
+    while (count > 0) {
+        repeated += original;
+        count--;
+    }
+    return repeated;
 }
 
 module.exports = repeatString
