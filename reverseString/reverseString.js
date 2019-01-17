b70d9ed0278b387e1a31ee34611e27187e6ab682
Ricky McCallum <rickymccallum87@gmail.com>
2019-01-17 18:27:48 -0500

    Reverse string

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(fString) {
+    fArray = fString.split('');
+    let rArray = [];
+    rArray = fArray.reverse();
+    // while (fArray.length > 0) {
+    //     rArray.push(fArray.pop());
+    // }
+    return rArray.join('');
 }
 
 module.exports = reverseString
