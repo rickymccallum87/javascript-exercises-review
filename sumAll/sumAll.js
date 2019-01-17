9a8ac9d2c5477ee0ae06b2111faeed8ed24dc9dc
Ricky McCallum <rickymccallum87@gmail.com>
2019-01-17 18:28:09 -0500

    Sum all

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0 ||
+                typeof start !== "number" ||
+                typeof end !== "number") {
+        return 'ERROR';
+    }
 
+    if (start > end) {
+        [start, end] = [end, start];
+    }
+
+    let total = start;
+
+    for (let i = start + 1; i <= end; i++) {
+        total += i;
+    }
+
+    return total;
 }
 
 module.exports = sumAll
